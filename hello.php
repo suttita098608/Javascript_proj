3cc2462a8794b68517ffb0a26167aa5a1f9ecf50
Suttita Natanreeb <suttita.n@ku.th>
2022-09-12 22:14:21 +0700

    add javascript function

---
+++ hello.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<h1 align="center">DIOR MAKEUP</h1>
+<?php
+ $str_a = "DIOR FOREVER";
+ $str_b = "CAPTURE TOTALE";
+ $str_c = "DIOR PRESTIGE";
+ $str_d = "DIOR FOREVER SKIN GLOW";
+ $str_e = "DIORSKIN FOREVER PERFECT MOUSSE";
+ $str_f = "DIOR FOREVER COUTURE SKIN GLOW";
+ $str_g = "DIORSKIN FOREVER GLOW CUSHION REFILL";
+ $str_h = "DIOR FOREVER CUSHION POWDER";
+ $str_i = "DIOR PRESTIGE";
+ $str_j = "DIOR FOREVER SKIN VEIL SPF20";
+ echo '
+    <table border="1" width="100%" cellspacing ="1">
+        <tr>
+          <td><div align="center">ID</div></td>
+          <td><center>PRODUCT</center></td>
+          <td><div align="center">NAME PRODUCT</div></td>
+          <td><div align="center">PRICE</div></td>
+        </tr>
+        <tr>
+          <td><div align="center">1</div></td>
+          <td><center><img border="0" src="01.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_a.'</div></td>
+          <td><div align="center">2550 Bath</div></td>
+        </tr>
+        <tr>
+          <td><div align="center">2</div></td>
+          <td><center><img border="0" src="02.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_b.'</div></td>
+          <td><div align="center">3250 Bath</div></td>
+        </tr>
+        <tr>
+          <td><div align="center">3</div></td>
+          <td><center><img border="0" src="03.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_c.'</div></td>
+          <td><div align="center">4300 Bath</div></td>
+        </tr>
+        <tr>
+          <td><div align="center">4</div></td>
+          <td><center><img border="0" src="04.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_d.'</div></td>
+          <td><div align="center">2600 Bath</div></td>
+        </tr>
+        <tr>
+          <td><div align="center">5</div></td>
+          <td><center><img border="0" src="05.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_e.'</div></td>
+          <td><div align="center">1750 Bath</div></td>
+        </tr>
+        <tr>
+          <td><div align="center">6</div></td>
+          <td><center><img border="0" src="06.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_f.'</div></td>
+          <td><div align="center">2750 Bath</div></td>
+        </tr>
+        <tr>
+          <td><div align="center">7</div></td>
+          <td><center><img border="0" src="07.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_g.'</div></td>
+          <td><div align="center">1650 Bath</div></td>
+        </tr>
+        <tr>
+          <td><div align="center">8</div></td>
+          <td><center><img border="0" src="08.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_h.'</div></td>
+          <td><div align="center">2350 Bath</div></td>
+        </tr>
+        <tr>
+          <td><div align="center">9</div></td>
+          <td><center><img border="0" src="09.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_i.'</div></td>
+          <td><div align="center">4500 Bath</div></td>
+        </tr>
+        <tr>
+        <td><div align="center">10</div></td>
+          <td><center><img border="0" src="10.jpg" width="200" height="200"></center></td>
+          <td><div align="center">'.$str_j.'</div></td>
+          <td><div align="center">2150 Bath</div></td>
+        </tr>
+    </table> ';
+?>
+</body>
+</html>
